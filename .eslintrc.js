2eedbdf9462a2d7c0d4a59d2d80e3263d36de609
Iuliia Dutchak <Iuliia_Dutchak@epam.com>
2017-09-06 16:31:02 +0200

    task 1: change eslintr config

---
+++ .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:import/errors",
+    "plugin:import/warnings",
+    "plugin:import/react",
+    "plugin:jsx-a11y/recommended"
+  ],
+  "settings": {
+    "import/resolver": {
+        "node": {
+          "extensions": [
+            ".js",
+            ".jsx"
+          ]
+        }
+      }
+  },
+  "parserOptions": {
+    "sourceType": "module",
+    "ecmaVersion": 8
+  },
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "rules": {
+    "jsx-quotes": [2, "prefer-single"],
+  },
+  "plugins": [
+    "react",
+    "import",
+    "jsx-a11y"
+  ],
+  "overrides": [
+    {
+      files: ["*.config.js"],
+      "env": {
+        "node": true,
+        "browser": false
+      }
+    }
+  ]
+};
