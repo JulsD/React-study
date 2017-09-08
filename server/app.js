390177ef2fc8c448fb381fcb1263af9130aeabf4
Iuliia Dutchak <Iuliia_Dutchak@epam.com>
2017-09-08 10:46:30 +0200

    task 1: set server app.js in server folder

--- server/app.js
+++ server/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const path = require('path');
+
 const app = express();
 
+app.use( express.static( __dirname + '/../dist/' ));
+
 app.get('/', function (req, res) {
-  res.sendFile(__dirname + '/dist/index.html');
+  res.sendFile(path.join( __dirname, '/../dist/', 'index.html' ));
 });
 
 app.listen(3000, function () {
