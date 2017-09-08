56f4a1f07e7696c14bfbb046576bfecf0959f997
Iuliia Dutchak <Iuliia_Dutchak@epam.com>
2017-09-08 11:42:28 +0200

    task 1: remove unnecessary code from server/app.js

--- server/app.js
+++ server/app.js
@@ -3,11 +3,7 @@ const path = require('path');
 
 const app = express();
 
-app.use( express.static( __dirname + '/../dist/' ));
-
-app.get('/', function (req, res) {
-  res.sendFile(path.join( __dirname, '/../dist/', 'index.html' ));
-});
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.listen(3000, function () {
   console.log('Приклад застосунку, який прослуховує 3000-ий порт!');
