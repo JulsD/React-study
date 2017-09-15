22438f8210eaed77a043df068665c6e2c879cacc
Iuliia Dutchak <Iuliia_Dutchak@epam.com>
2017-09-15 15:47:10 +0200

    task 2: add home-page component

---
+++ src/app/HomePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class HomePage extends React.Component {
+
+  render() {
+    const style = {
+      width: '100%',
+      backgroundColor: 'yellow'
+    };
+    return (
+      <div style={style}>
+        HomePage
+      </div>
+    );
+  }
+
+}
+
+export default HomePage;
--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import {render} from 'react-dom';
 
-import AwesomeComponent from './AwesomeComponent';
+import HomePage from './HomePage';
 
 class App extends React.Component {
   render () {
     return (
       <div>
-        <p> Hello React!</p>
-        <AwesomeComponent />
+        <HomePage />
       </div>
     );
   }
