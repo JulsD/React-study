764dd61a84ce50cead6e5005d64d503024f119f5
JulsD <dutchak.julia@gmail.com>
2017-10-23 23:50:00 +0200

    wrapp in redax provider main App component

--- src/app.jsx
+++ src/app.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import { render } from 'react-dom';
+import { Provider } from 'react-redux'
 import { AppContainer } from 'react-hot-loader'
 import { BrowserRouter } from 'react-router-dom';
 
 import './styles/common.css';
 import App from './app/index.jsx';
+import configureStore from './app/store/configureStore'
+
+const store = configureStore()
 
 render((
-  <AppContainer>
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
-  </AppContainer>
+  <Provider store={store}>
+    <AppContainer>
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+    </AppContainer>
+  </Provider>
 ), document.getElementById('app'));
