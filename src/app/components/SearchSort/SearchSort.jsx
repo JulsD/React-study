8b61d545d198d1b5b457f1fc9842b6ba7cf1f179
JulsD <dutchak.julia@gmail.com>
2017-10-27 02:44:20 +0200

    fix searchSort according to new DB properties names

--- src/app/components/SearchSort/SearchSort.jsx
+++ src/app/components/SearchSort/SearchSort.jsx
@@ -7,7 +7,7 @@ class SearchSort extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      sortBy: 'release_year'
+      sortBy: 'release_date'
     };
 
     this.handleInputChange = this.handleInputChange.bind(this);
@@ -17,10 +17,6 @@ class SearchSort extends React.Component {
     let target = event.target;
     let value = target.value;
     let name = target.name;
-
-    if (value == 'release date') {
-      value = 'release_year';
-    }
 
     this.setState({
       [name]: value
@@ -33,21 +29,21 @@ class SearchSort extends React.Component {
     return (
       <div className = {styles.root}>
         <h4>Sort by</h4>
-        <label className = {this.state.sortBy ==='release_year' ? styles.active : ''}>
+        <label className = {this.state.sortBy ==='release_date' ? styles.active : ''}>
           release date
           <input type='radio'
                  name='sortBy'
                  value='release date'
                  onChange={this.handleInputChange}
-                 checked={this.state.sortBy ==='release_year'}/>
+                 checked={this.state.sortBy ==='release_date'}/>
         </label>
-        <label className = {this.state.sortBy ==='rating' ? styles.active : ''}>
+        <label className = {this.state.sortBy ==='vote_average' ? styles.active : ''}>
           rating
           <input type='radio'
                  name='sortBy'
-                 value='rating'
+                 value='vote_average'
                  onChange={this.handleInputChange}
-                 checked={this.state.sortBy ==='rating'} />
+                 checked={this.state.sortBy ==='vote_average'} />
         </label>
       </div>
     );
