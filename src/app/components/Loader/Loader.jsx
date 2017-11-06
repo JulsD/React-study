2430973e4407724e994251f19747eb545fcc186f
JulsD <dutchak.julia@gmail.com>
2017-11-06 21:58:38 +0100

    add Loader component

---
+++ src/app/components/Loader/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './loader.css';
+
+class Loader extends React.Component {
+
+  render() {
+    return (
+      <div className = {styles.root}></div>
+    );
+  }
+
+}
+
+export default Loader;
